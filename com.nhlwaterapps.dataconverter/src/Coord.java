0b110f965be08f6496203623a2ea55a6372d4953
Marten Schilstra <martenschilstra@gmail.com>
2012-05-23 13:26:56 +0200

    DataConverter \0/

---
+++ com.nhlwaterapps.dataconverter/src/Coord.java
@@ -0,0 +1,9 @@
+public class Coord {
+	public double latitude;
+	public double longitude;
+	
+	public Coord(double _latitude, double _longitude) {
+		this.latitude = _latitude;
+		this.longitude = _longitude;
+	}
+}
